c7add830d4db9216d56db45f26579e19aaf2c50f
Karl Parton <karlparton@gmail.com>
2025-09-30 13:15:25 +0100

    additional customer journeys.

--- app/routes.js
+++ app/routes.js
@@ -16,20 +16,57 @@ router.post("/parcel-registered", function (req, res) {
     res.redirect("know-parcel-id");
     }
     else if (req.session.data["parcelRegistered"] == "dont-know") {
-    res.redirect("register-land-start");
+    res.redirect("check-if-parcel-id-registered");
   }
 });
+
+// router.post("/know-parcel-id", function (req, res) {
+//   if (req.session.data["knowParcelID"] == "yes") {
+//     res.redirect("enter-parcel-id");
+//   } else if (req.session.data["knowParcelID"] == "no") {
+//     res.redirect("find-parcel-id");
+//   }
+// });
 
 router.post("/know-parcel-id", function (req, res) {
-  if (req.session.data["knowParcelID"] == "yes") {
+  if (req.session.data["parcelRegistered"] == "yes" && req.session.data["knowParcelID"] == "yes" || req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "yes") {
     res.redirect("enter-parcel-id");
-  } else if (req.session.data["knowParcelID"] == "no") {
-    res.redirect("register-land-start");
   }
+    // if (req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "yes") {
+    // res.redirect("enter-parcel-id");
+    // }
+    else if (req.session.data["parcelRegistered"] == "yes" && req.session.data["knowParcelID"] == "no") {
+    res.redirect("find-parcel-id");
+  }
+
+    else if (req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "no") {
+    res.redirect("estimate-parcel-id");
+  }
+
 });
 
+// router.post("/enter-parcel-id", function (req, res) {
+//   res.redirect("parcel-is-registered");
+// });
+
+// router.post("/enter-parcel-id", function (req, res) {
+//   if (req.session.data["parcelID"] == "TL42570684") {
+//     res.redirect("parcel-is-registered");
+//   } else {
+//     res.redirect("parcel-is-not-registered");
+//   }
+// });
+
 router.post("/enter-parcel-id", function (req, res) {
-  res.redirect("parcel-is-registered");
+  if (req.session.data["parcelID"] == "TL42570684") {
+    res.redirect("parcel-is-registered");
+  }
+    else if (req.session.data["parcelID"] == "CS07717013") {
+    res.redirect("parcel-is-new");
+    }
+    else {
+    res.redirect("parcel-is-not-registered");
+  }
 });
 
 router.post("/parcel-is-registered", function (req, res) {
@@ -37,10 +74,36 @@ router.post("/parcel-is-registered", function (req, res) {
 });
 
 router.post("/date-to-link-parcel-to-business", function (req, res) {
+
+  let parcel = {
+    'registered': req.session.data['parcelRegistered'],
+    'know': req.session.data['knowParcelID'],
+    'id': req.session.data['parcelID'],
+    'day': req.session.data['date-to-link-day'],
+    'month': req.session.data['date-to-link-month'],
+    'year': req.session.data['date-to-link-year']
+  }
+
+// set up an empty array in case the credit choices object doesn't exist yet
+  let currentParcelChoices = []
+  if (req.session.data['parcel-choices']) {
+    currentParcelChoices = req.session.data['parcel-choices']
+  }
+
+  currentParcelChoices.push(parcel)
+
+  // update the credit choices in the session data
+  req.session.data['parcel-choices'] = currentParcelChoices
+  console.log('ParcelChoices Array ' + JSON.stringify(req.session.data['parcel-choices']))
+
+
   res.redirect("check-parcel-details");
 });
 
 router.post("/check-parcel-details", function (req, res) {
+
+
+
   if (req.session.data["registerAnotherParcel"] == "yes") {
     res.redirect("parcel-registered");
   } else if (req.session.data["registerAnotherParcel"] == "no") {
@@ -51,3 +114,77 @@ router.post("/check-parcel-details", function (req, res) {
 router.post("/register-land-declaration", function (req, res) {
   res.redirect("register-land-confirmation");
 });
+
+router.post("/parcel-is-not-registered", function (req, res) {
+  res.redirect("upload-land-parcel-map");
+});
+
+router.post("/upload-land-parcel-map", function (req, res) {
+  res.redirect("date-to-link-parcel-to-business");
+});
+
+// router.post("/find-parcel-id", function (req, res) {
+//   res.redirect("parcel-is-registered");
+// });
+
+router.post("/find-parcel-id", function (req, res) {
+  if (req.session.data["foundParcelID"] == "TL42570684") {
+    res.redirect("parcel-is-registered");
+  } else {
+    res.redirect("parcel-is-not-registered");
+  }
+});
+
+router.post("/parcel-is-new", function (req, res) {
+  res.redirect("upload-land-parcel-map");
+});
+
+router.post("/estimate-parcel-ID", function (req, res) {
+  res.redirect("parcel-is-new");
+});
+
+router.post("/check-if-parcel-id-registered", function (req, res) {
+  if (req.session.data["foundParcelID"] == "TL42570684") {
+    res.redirect("parcel-is-registered");
+  } else {
+    res.redirect("parcel-is-not-registered");
+  }
+});
+
+// Known land parcel - TL42570684
+// Register new land parcel - CS07717013
+// Land parcel not registered - RD69202871
+
+
+// router.post('/credits-add-more', function (req, res) {
+  
+//   let credit = {
+//     'type': req.session.data['credits-type'],
+//     'tier': req.session.data['credits-tier'],
+//     'distinctiveness': req.session.data['credits-distinctiveness'],
+//     'broad': req.session.data['credits-broad-habitat-type'],
+//     'habitat': req.session.data['credits-habitat-type'],
+//     'number': req.session.data['credits-number']
+//   }
+
+//   // set up an empty array in case the credit choices object doesn't exist yet
+//   let currentCreditChoices = []
+//   if (req.session.data['credit-choices']) {
+//     currentCreditChoices = req.session.data['credit-choices']
+//   }
+
+//   currentCreditChoices.push(credit)
+
+//   // update the credit choices in the session data
+//   req.session.data['credit-choices'] = currentCreditChoices
+//   console.log('CreditChoices Array ' + JSON.stringify(req.session.data['credit-choices']))
+  
+//   if (req.session.data['add-more-credits'] == 'yes') {
+//     //req.session.data = {}
+//     res.redirect('credits-type');
+    
+//   }
+//   else if (req.session.data['add-more-credits'] == 'no') {
+//     res.redirect('credits-estimated-cost');
+//   }
+// });
