19e1e8d1f0011b7928cfb24d66df00fa5075a921
Karl Parton <karlparton@gmail.com>
2025-10-10 11:29:53 +0100

    Tidied up routes.

--- app/views/register-land-v2/_routes.js
+++ app/views/register-land-v2/_routes.js
@@ -8,39 +8,8 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-router.post("/register-land-v2/parcel-registered", function (req, res) {
-  if (req.session.data["parcelRegistered"] == "yes") {
-    res.redirect("know-parcel-id");
-  }
-    if (req.session.data["parcelRegistered"] == "no") {
-    res.redirect("know-parcel-id");
-    }
-    else if (req.session.data["parcelRegistered"] == "dont-know") {
-    res.redirect("check-if-parcel-id-registered");
-  }
-});
 
-// router.post("/know-parcel-id", function (req, res) {
-//   if (req.session.data["knowParcelID"] == "yes") {
-//     res.redirect("enter-parcel-id");
-//   } else if (req.session.data["knowParcelID"] == "no") {
-//     res.redirect("find-parcel-id");
-//   }
-// });
-
-// router.post("/register-land-v2/know-parcel-id", function (req, res) {
-//   if (req.session.data["parcelRegistered"] == "yes" && req.session.data["knowParcelID"] == "yes" || req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "yes") {
-//     res.redirect("enter-parcel-id");
-//   }
-//     else if (req.session.data["parcelRegistered"] == "yes" && req.session.data["knowParcelID"] == "no") {
-//     res.redirect("find-parcel-id");
-//   }
-
-//     else if (req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "no") {
-//     res.redirect("estimate-parcel-id");
-//   }
-
-// });
+// Do you know the Parcel ID?
 
 router.post("/register-land-v2/know-parcel-id", function (req, res) {
   if (req.session.data["knowParcelID"] == "yes") {
@@ -50,34 +19,7 @@ router.post("/register-land-v2/know-parcel-id", function (req, res) {
   }
 });
 
-// router.post("/register-land-v2/know-parcel-id", function (req, res) {
-//   if (req.session.data["parcelRegistered"] == "yes" && req.session.data["knowParcelID"] == "yes" || req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "yes") {
-//     res.redirect("enter-parcel-id");
-//   }
-//     // if (req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "yes") {
-//     // res.redirect("enter-parcel-id");
-//     // }
-//     else if (req.session.data["parcelRegistered"] == "yes" && req.session.data["knowParcelID"] == "no") {
-//     res.redirect("find-parcel-id");
-//   }
-
-//     else if (req.session.data["parcelRegistered"] == "no" && req.session.data["knowParcelID"] == "no") {
-//     res.redirect("estimate-parcel-id");
-//   }
-
-// });
-
-// router.post("/enter-parcel-id", function (req, res) {
-//   res.redirect("parcel-is-registered");
-// });
-
-// router.post("/enter-parcel-id", function (req, res) {
-//   if (req.session.data["parcelID"] == "TL42570684") {
-//     res.redirect("parcel-is-registered");
-//   } else {
-//     res.redirect("parcel-is-not-registered");
-//   }
-// });
+// Enter the Parcel ID 
 
 router.post("/register-land-v2/enter-parcel-id", function (req, res) {
   if (req.session.data["parcelID"] == "TL4257 0684") {
@@ -90,6 +32,8 @@ router.post("/register-land-v2/enter-parcel-id", function (req, res) {
     res.redirect("parcel-is-not-registered");
   }
 });
+
+// If land parcel is registered
 
 router.post("/register-land-v2/parcel-is-registered", function (req, res) {
   res.redirect("date-to-link-parcel-to-business");
@@ -106,7 +50,8 @@ router.post("/register-land-v2/date-to-link-parcel-to-business", function (req, 
     'year': req.session.data['date-to-link-year']
   }
 
-// set up an empty array in case the credit choices object doesn't exist yet
+  // set up an empty array in case the parcel choices object doesn't exist yet
+
   let currentParcelChoices = []
   if (req.session.data['parcel-choices']) {
     currentParcelChoices = req.session.data['parcel-choices']
@@ -122,10 +67,9 @@ router.post("/register-land-v2/date-to-link-parcel-to-business", function (req, 
   res.redirect("check-parcel-details");
 });
 
+// Ask user if they'd like add additional parcels if yes begin process of adding parcel if not send to declaration
+
 router.post("/register-land-v2/check-parcel-details", function (req, res) {
-
-
-
   if (req.session.data["registerAnotherParcel"] == "yes") {
     res.redirect("know-parcel-id");
   } else if (req.session.data["registerAnotherParcel"] == "no") {
@@ -133,55 +77,46 @@ router.post("/register-land-v2/check-parcel-details", function (req, res) {
   }
 });
 
+// From declaration send to confirmation
+
 router.post("/register-land-v2/register-land-declaration", function (req, res) {
   res.redirect("register-land-confirmation");
 });
 
+// If parcel not send to map to estimate parcel ID
+
 router.post("/register-land-v2/parcel-is-not-registered", function (req, res) {
-  res.redirect("upload-land-parcel-map");
+  res.redirect("find-parcel-ID");
 });
+
+// When map uploaded send to date to link parcel to business
 
 router.post("/register-land-v2/upload-land-parcel-map", function (req, res) {
   res.redirect("date-to-link-parcel-to-business");
 });
 
-// router.post("/find-parcel-id", function (req, res) {
-//   res.redirect("parcel-is-registered");
-// });
-
-router.post("/register-land-v2/find-parcel-id", function (req, res) {
-  if (req.session.data["foundParcelID"] == "TL4257 0684") {
-    res.redirect("parcel-is-registered");
-  } else {
-    res.redirect("parcel-is-not-registered");
-  }
-});
-
-// router.post("/register-land-v2/parcel-is-new", function (req, res) {
-//   res.redirect("upload-land-parcel-map");
-// });
+// If parcel is new send to map to find parcel ID
 
 router.post("/register-land-v2/parcel-is-new", function (req, res) {
   res.redirect("find-parcel-id");
 });
 
-router.post("/register-land-v2/estimate-parcel-ID", function (req, res) {
-  res.redirect("parcel-is-new");
-});
+// If parcel is new and already registered send to add date to link, if parcel is not registered send to map upload
 
-router.post("/register-land-v2/check-if-parcel-id-registered", function (req, res) {
-  if (req.session.data["foundParcelID"] == "TL4257 0684") {
+router.post("/register-land-v2/find-parcel-id", function (req, res) {
+  if (req.session.data["parcelID"] == "CS0771 7013") {
     res.redirect("parcel-is-registered");
   } else {
-    res.redirect("parcel-is-not-registered");
+    res.redirect("upload-land-parcel-map");
   }
 });
 
-// Known land parcel - TL42570684
-// Register new land parcel - CS07717013
-// Land parcel not registered - RD69202871
 
+// Known land parcel - TL4257 0684
+// Register new land parcel - CS0771 7013
+// Land parcel not registered - RD6920 2871
 
+// Code for add to list
 // router.post('/credits-add-more', function (req, res) {
   
 //   let credit = {
