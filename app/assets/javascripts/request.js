29d73b03ddd2f599026901da213d681a04b20391
Karl Parton <karlparton@gmail.com>
2025-11-28 18:38:28 +0000

    Update request.js

--- app/assets/javascripts/request.js
+++ app/assets/javascripts/request.js
@@ -1,5 +1,29 @@
-const osAuth = {}
+// const osAuth = {}
 
+// const transformTileRequest = (url, resourceType) => {
+//   // Only proxy OS API requests that don't already have a key
+//   if (resourceType !== 'Style' && url.startsWith('https://api.os.uk')) {
+//     const urlObj = new URL(url)
+//     if (!urlObj.searchParams.has('key')) {
+//       return {
+//         url: `/api/map-proxy?url=${encodeURIComponent(url)}`,
+//         headers: {}
+//       }
+//     }
+//   }
+//   return { url, headers: {} }
+// }
+
+// const transformDataRequest = (request, bbox) => {
+//   return {
+//     url: `${env.WFS_SERVICE_URL}?wfs_url=${encodeURIComponent(request.url)}&bbox=${bbox}`
+//   }
+// }
+
+// window.transformTileRequest = transformTileRequest
+// window.transformDataRequest = transformDataRequest
+
+const osAuth = {}
 const transformTileRequest = (url, resourceType) => {
   // Only proxy OS API requests that don't already have a key
   if (resourceType !== 'Style' && url.startsWith('https://api.os.uk')) {
@@ -15,8 +39,13 @@ const transformTileRequest = (url, resourceType) => {
 }
 
 const transformDataRequest = (request, bbox) => {
+  // Detect if we're on localhost or production
+  const proxyBase = window.location.hostname === 'localhost' 
+    ? 'http://localhost:8000'
+    : 'https://farming-data-7db3d1889632.herokuapp.com';
+  
   return {
-    url: `${env.WFS_SERVICE_URL}?wfs_url=${encodeURIComponent(request.url)}&bbox=${bbox}`
+    url: `${proxyBase}/wfs_geojson?wfs_url=${encodeURIComponent(request.url)}&bbox=${bbox}`
   }
 }
 
