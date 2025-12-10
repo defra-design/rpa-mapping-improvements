10385e5b7c1e98a6ee353d3393d45f8833ff502c
Karl Parton <karlparton@gmail.com>
2025-12-10 10:44:30 +0000

    Fix to get confirm parcel screen working on Heroku. Added v4 of get maps of your land.

--- app/views/register-land-v3/_routes.js
+++ app/views/register-land-v3/_routes.js
@@ -32,7 +32,7 @@ router.post("/register-land-v3/know-parcel-id", function (req, res) {
     }
     
     // Redirect to the confirmation page (which will display the summary list)
-    res.redirect("confirm-land-parcel-single");
+    res.redirect("confirm-land-parcel-single-full-width");
     
   } else {
     // Redirect to the alternative journey (estimate land parcel)
@@ -41,7 +41,7 @@ router.post("/register-land-v3/know-parcel-id", function (req, res) {
 });
 
 // Confirm land parcel
-router.post("/register-land-v3/confirm-land-parcel-single", function (req, res) {
+router.post("/register-land-v3/confirm-land-parcel-single-full-width", function (req, res) {
   res.redirect("date-to-link-parcel-to-business");
 });
 
