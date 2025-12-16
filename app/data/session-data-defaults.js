31b1ab32047167e289c27a8a0025ce95f9dbb0c1
Karl Parton <karlparton@gmail.com>
2025-12-16 11:19:19 +0000

    Fixed config.

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -4,7 +4,6 @@ module.exports = {
   VTS_DARK_URL: process.env.VTS_DARK_URL,
   VTS_BLACK_AND_WHITE_URL: process.env.VTS_BLACK_AND_WHITE_URL,
   VTS_SATELLITE_URL: process.env.VTS_SATELLITE_URL,
-
   // OS Names API 
   OS_NAMES_URL: process.env.OS_NAMES_URL,
   OS_NEAREST_URL: process.env.OS_NEAREST_URL,
@@ -12,8 +11,11 @@ module.exports = {
   GRIDREF_SERVICE_URL: process.env.GRIDREF_SERVICE_URL,
   // WFS Conversion
   WFS_SERVICE_URL: process.env.WFS_SERVICE_URL,
-  WFS_DATA_URL: process.env.WFS_DATA_URL,
   PARCEL_SERVICE_URL: process.env.PARCEL_SERVICE_URL,
+  // WFS End points
+  WFS_DATA_URL: process.env.WFS_DATA_URL,
+  WFS_HEDGEROWS_URL: process.env.WFS_HEDGEROWS_URL,
+  WFS_LANDCOVERS_URL: process.env.WFS_LANDCOVERS_URL,
   // OS API Key
   OS_API_KEY: process.env.OS_API_KEY
 }
